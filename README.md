67a4973baf7031188c7f5cacc5ca372d6a2aa3a0
Ben Formosa <ben.formosa@gmail.com>
2018-08-24 11:31:01 +1000

    apply style

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # sat6-currency
-Satellite 6 version of 'spacewalk-report system-currency'. 
+Satellite 6 version of 'spacewalk-report system-currency'
 
 # Requirements
 
--- sat6-currency.py
+++ sat6-currency.py
@@ -227,9 +227,11 @@ def simple_currency():
                 score = 0
             else:
                 # Calculate weighted score
-                score = (errata_count_sec * factor_sec +
-                         errata_count_bug * factor_bug +
-                         errata_count_enh * factor_enh)
+                score = (
+                    errata_count_sec * factor_sec +
+                    errata_count_bug * factor_bug +
+                    errata_count_enh * factor_enh
+                )
 
             # Print result
             print(
@@ -348,12 +350,14 @@ def advanced_currency():
                         errata_count_bug += 1
 
             # Calculate weighted score
-            score = (factor_cri * errata_count_cri +
-                     factor_imp * errata_count_imp +
-                     factor_mod * errata_count_mod +
-                     factor_low * errata_count_low +
-                     factor_bug * errata_count_bug +
-                     factor_enh * errata_count_enh)
+            score = (
+                factor_cri * errata_count_cri +
+                factor_imp * errata_count_imp +
+                factor_mod * errata_count_mod +
+                factor_low * errata_count_low +
+                factor_bug * errata_count_bug +
+                factor_enh * errata_count_enh
+            )
 
             # Print result
             print(
@@ -388,17 +392,29 @@ def library_currency():
         sys.exit(1)
 
     # Print headline
-    print "system_id,org_name,name,total_available_security,critical,important,moderate,low,bug,enhancement,score,total_applicable_security,applicable_critical,applicable_important,applicable_moderate,applicable_low,applicable_bug,applicable_enhancement,applicable_score,content_view,content_view_publish_date,lifecycle_environment,subscription_os_release,os_release,arch,subscription_status,comment"
+    print(
+        "system_id,org_name,name,total_available_security,critical,"
+        "important,moderate,low,bug,enhancement,score,"
+        "total_applicable_security,applicable_critical,applicable_important,"
+        "applicable_moderate,applicable_low,applicable_bug,"
+        "applicable_enhancement,applicable_score,content_view,"
+        "content_view_publish_date,lifecycle_environment,"
+        "subscription_os_release,os_release,arch,subscription_status,comment"
+          )
     # Open reports files
     available_file = open('available_errata.csv', 'w')
-    available_file.write('system_id,org_name,name,state,errata_id,issued,updated,severity,type,reboot_suggested,title,further_info\n')
+    available_file.write(
+        "system_id,org_name,name,state,errata_id,issued,"
+        "updated,severity,type,reboot_suggested,title,further_info\n"
+    )
     applicable_file = open('applicable_errata.csv', 'w')
-    applicable_file.write('system_id,org_name,name,state,errata_id,issued,updated,severity,type,reboot_suggested,title,further_info\n')
+    applicable_file.write(
+        "system_id,org_name,name,state,errata_id,issued,"
+        "updated,severity,type,reboot_suggested,title,further_info\n"
+    )
 
     # Red Hat errata URL
     RH_URL = "https://access.redhat.com/errata/"
-    # Fedora EPEL errata URL
-    EPEL_URL = "https://bodhi.fedoraproject.org/updates"
 
     # Find organization
     organization = get_with_json(
@@ -409,17 +425,28 @@ def library_currency():
     # print str(organization_id)
 
     # Find lifecycle_environment
-    lifecycle_environment_compare = get_with_json(katello_api + "/organizations/" + str(organization_id) + "/environments?name=" + args.environment, json.dumps({"per_page": "10000"}))["results"]
+    lifecycle_environment_compare = get_with_json(
+        "{}/organizations/{}/environments?name={}".format(
+            katello_api, str(organization_id), args.environment),
+        json.dumps({"per_page": "10000"})
+    )["results"]
     lifecycle_environment_compare_id = lifecycle_environment_compare[0]["id"]
     # print str(lifecycle_environment_compare_id)
 
     # Find content view
-    content_view_compare = get_with_json(katello_api + "/organizations/" + str(organization_id) + "/content_views?name=" + args.contentview, json.dumps({"per_page": "10000"}))["results"]
+    content_view_compare = get_with_json(
+        "{}/organizations/{}/content_views?name={}".format(
+            katello_api, str(organization_id), args.contentview),
+        json.dumps({"per_page": "10000"})
+    )["results"]
     content_view_compare_id = content_view_compare[0]["id"]
     # print str(content_view_compare_id)
 
-    # Get all hosts (if you have more than 10000 hosts, this method will take too long itme)
-    hosts = get_with_json(api + "hosts", json.dumps({"per_page": "10000"}))["results"]
+    # Get all hosts (for more than 10000 hosts, this will take a long time)
+    hosts = get_with_json(
+        "{}hosts{}".format(api, args.search),
+        json.dumps({"per_page": "10000"})
+    )["results"]
 
     # Multiply factors according to "spacewalk-report system-currency"
     factor_cri = 32
@@ -432,10 +459,22 @@ def library_currency():
     for host in hosts:
 
         # Get all errata for each host
-        erratas = get_with_json(api + "hosts/" + str(host["id"]) + "/errata", json.dumps({"per_page": "10000"}))
-        applicable_erratas = get_with_json(api + "hosts/" + str(host["id"]) + "/errata?environment_id=" + str(lifecycle_environment_compare_id) + "&content_view_id=" + str(content_view_compare_id), json.dumps({"per_page": "10000"}))
+        erratas = get_with_json(
+            "{}hosts/{}/errata".format(api, str(host["id"])),
+            json.dumps({"per_page": "10000"})
+        )
+        applicable_erratas = get_with_json(
+            "{}hosts/{}/errata?environment_id={}&content_view_id={}".format(
+                api,
+                str(host["id"]),
+                str(lifecycle_environment_compare_id),
+                str(content_view_compare_id)
+            ),
+            json.dumps({"per_page": "10000"})
+        )
 
-        # Check if host is registered with subscription-manager (unregistered hosts lack these values and are skipped)
+        # Check if host is registered with subscription-manager
+        # (unregistered hosts lack these values and are skipped)
         if "results" in erratas:
 
             errata_count_sec = 0
@@ -455,17 +494,35 @@ def library_currency():
             applicable_errata_count_bug = 0
 
             # Check if host have any errrata at all
-            if "total" in erratas and "content_facet_attributes" in host and "subscription_facet_attributes" in host:
-                content_view_name = host["content_facet_attributes"]["content_view"]["name"]
-                content_view_id = host["content_facet_attributes"]["content_view"]["id"]
-                lifecycle_environment = host["content_facet_attributes"]["lifecycle_environment"]["name"]
-                lifecycle_environment_id = host["content_facet_attributes"]["lifecycle_environment"]["id"]
-                subscription_os_release= host["subscription_facet_attributes"]["release_version"]
+            if (
+                    "total" in erratas and
+                    "content_facet_attributes" in host and
+                    "subscription_facet_attributes" in host
+            ):
+                content_view_name = host[
+                    "content_facet_attributes"]["content_view"]["name"]
+                content_view_id = host[
+                    "content_facet_attributes"]["content_view"]["id"]
+                lifecycle_environment = host[
+                    "content_facet_attributes"][
+                        "lifecycle_environment"]["name"]
+                lifecycle_environment_id = host[
+                    "content_facet_attributes"]["lifecycle_environment"]["id"]
+                subscription_os_release = host[
+                    "subscription_facet_attributes"]["release_version"]
                 arch = host["architecture_name"]
                 subscription_status = host["subscription_status"]
                 os_release = host["operatingsystem_name"]
 
-                content_view = get_with_json(katello_api + "/content_views/" + str(content_view_id) + "/content_view_versions?environment_id=" + str(lifecycle_environment_id), json.dumps({"per_page": "10000"}))["results"]
+                content_view = get_with_json(
+                    "{}/content_views/{}/content_view_versions?"
+                    "environment_id={}".format(
+                        katello_api,
+                        str(content_view_id),
+                        str(lifecycle_environment_id)
+                    ),
+                    json.dumps({"per_page": "10000"})
+                )["results"]
 
                 cv_date = content_view[0]["created_at"]
 
@@ -475,48 +532,137 @@ def library_currency():
                     # If it is a security errata, check the severity
                     if errata["type"] == "security":
                         errata_count_sec += 1
-                        if errata["severity"] == "Critical": errata_count_cri += 1
-                        if errata["severity"] == "Important": errata_count_imp += 1
-                        if errata["severity"] == "Moderate": errata_count_mod += 1
-                        if errata["severity"] == "Low": errata_count_low += 1
+                        if errata["severity"] == "Critical":
+                            errata_count_cri += 1
+                        if errata["severity"] == "Important":
+                            errata_count_imp += 1
+                        if errata["severity"] == "Moderate":
+                            errata_count_mod += 1
+                        if errata["severity"] == "Low":
+                            errata_count_low += 1
 
-                    if errata["type"] == "enhancement": errata_count_enh += 1
-                    if errata["type"] == "bugfix": errata_count_bug += 1
+                    if errata["type"] == "enhancement":
+                        errata_count_enh += 1
+                    if errata["type"] == "bugfix":
+                        errata_count_bug += 1
 
                     # Delete any commas from the errata title
                     # eg: https://access.redhat.com/errata/RHSA-2017:0817
                     errata["title"] = errata["title"].replace(',', '')
-                    available_file.write ( str(host["id"]) + "," + str(host["organization_name"]) + "," + host["name"] + ",Available," + str(errata["errata_id"]) + "," + str(errata["issued"]) + "," + str(errata["updated"]) + "," + str(errata["severity"]) + "," + str(errata["type"]) + "," + str(errata["reboot_suggested"]) + "," + str(errata["title"]) + "," + str(RH_URL) + str(errata["errata_id"]) + '\n')
+                    available_file.write(
+                        ','.join(str(x) for x in [
+                            str(host["id"]),
+                            str(host["organization_name"]),
+                            host["name"],
+                            "Available",
+                            str(errata["errata_id"]),
+                            str(errata["issued"]),
+                            str(errata["updated"]),
+                            str(errata["severity"]),
+                            str(errata["type"]),
+                            str(errata["reboot_suggested"]),
+                            str(errata["title"]),
+                            "{}{}\n".format(RH_URL, str(errata["errata_id"])),
+                        ]
+                                 )
+                    )
 
                 # Go through each errata that is applicable (in the library)
                 for errata in applicable_erratas["results"]:
 
                     # If it is a security errata, check the severity
                     if errata["type"] == "security":
-                        applicable_errata_count_sec +=1
-                        if errata["severity"] == "Critical": applicable_errata_count_cri += 1
-                        if errata["severity"] == "Important": applicable_errata_count_imp += 1
-                        if errata["severity"] == "Moderate": applicable_errata_count_mod += 1
-                        if errata["severity"] == "Low": applicable_errata_count_low += 1
+                        applicable_errata_count_sec += 1
+                        if errata["severity"] == "Critical":
+                            applicable_errata_count_cri += 1
+                        if errata["severity"] == "Important":
+                            applicable_errata_count_imp += 1
+                        if errata["severity"] == "Moderate":
+                            applicable_errata_count_mod += 1
+                        if errata["severity"] == "Low":
+                            applicable_errata_count_low += 1
 
-                    if errata["type"] == "enhancement": applicable_errata_count_enh += 1
-                    if errata["type"] == "bugfix": applicable_errata_count_bug += 1
+                    if errata["type"] == "enhancement":
+                        applicable_errata_count_enh += 1
+                    if errata["type"] == "bugfix":
+                        applicable_errata_count_bug += 1
 
                     # Delete any commas from the errata title
                     # eg: https://access.redhat.com/errata/RHSA-2017:0817
                     errata["title"] = errata["title"].replace(',', '')
-                    # print str(host["id"]) + "," + host["name"] + ",Applicable," + str(errata["errata_id"]) + "," + str(errata["issued"]) + "," + str(errata["updated"]) + "," + str(errata["severity"]) + "," + str(errata["type"]) + "," + str(errata["reboot_suggested"]) + "," + str(errata["updated"]) + "," + str(errata["title"]) + ",<a href=\"https://access.redhat.com/errata/" + str(errata["errata_id"]) + "\">" + str(errata["errata_id"]) + "</a>"
-                    applicable_file.write ( str(host["id"]) + "," + str(host["organization_name"]) + "," + host["name"] + ",Applicable," + str(errata["errata_id"]) + "," + str(errata["issued"]) + "," + str(errata["updated"]) + "," + str(errata["severity"]) + "," + str(errata["type"]) + "," + str(errata["reboot_suggested"]) + "," + str(errata["title"])  + "," + str(RH_URL) + str(errata["errata_id"]) + '\n')
+                    available_file.write(
+                        ','.join(str(x) for x in [
+                            str(host["id"]),
+                            str(host["organization_name"]),
+                            host["name"],
+                            "Applicable",
+                            str(errata["errata_id"]),
+                            str(errata["issued"]),
+                            str(errata["updated"]),
+                            str(errata["severity"]),
+                            str(errata["type"]),
+                            str(errata["reboot_suggested"]),
+                            str(errata["title"]),
+                            "{}{}\n".format(RH_URL, str(errata["errata_id"])),
+                        ]
+                                 )
+                    )
 
             # Calculate weighted score
-            score = factor_cri * errata_count_cri + factor_imp * errata_count_imp + factor_mod * errata_count_mod + factor_low * errata_count_low + factor_bug * errata_count_bug + factor_enh * errata_count_enh
-            applicable_score = factor_cri * applicable_errata_count_cri + factor_imp * applicable_errata_count_imp + factor_mod * applicable_errata_count_mod + factor_low * applicable_errata_count_low + factor_bug * applicable_errata_count_bug + factor_enh * applicable_errata_count_enh
+            score = (
+                factor_cri * errata_count_cri +
+                factor_imp * errata_count_imp +
+                factor_mod * errata_count_mod +
+                factor_low * errata_count_low +
+                factor_bug * errata_count_bug +
+                factor_enh * errata_count_enh
+            )
+            applicable_score = (
+                factor_cri * applicable_errata_count_cri +
+                factor_imp * applicable_errata_count_imp +
+                factor_mod * applicable_errata_count_mod +
+                factor_low * applicable_errata_count_low +
+                factor_bug * applicable_errata_count_bug +
+                factor_enh * applicable_errata_count_enh
+            )
 
             # Print result
-            print str(host["id"]) + "," + str(host["organization_name"]) + "," + host["name"] + "," + str(errata_count_sec) + "," + str(errata_count_cri) + "," + str(errata_count_imp) + "," + str(errata_count_mod) + "," + str(errata_count_low) + "," + str(errata_count_bug) + "," + str(errata_count_enh) + "," + str(score) + ","  + str(applicable_errata_count_sec) + "," + str(applicable_errata_count_cri) + "," + str(applicable_errata_count_imp) + "," + str(applicable_errata_count_mod) + "," + str(applicable_errata_count_low) + "," + str(applicable_errata_count_bug) + "," + str(applicable_errata_count_enh) + "," + str(applicable_score) + "," + str(content_view_name) + "," + str(cv_date) + "," + str(lifecycle_environment) + "," + str(subscription_os_release) + "," + str(os_release) + "," + str(arch) + "," + str(subscription_status) + "," + str(host["comment"])
+            print(
+                ','.join(str(x) for x in [
+                    str(host["id"]),
+                    str(host["organization_name"]),
+                    host["name"],
+                    str(errata_count_sec),
+                    str(errata_count_cri),
+                    str(errata_count_imp),
+                    str(errata_count_mod),
+                    str(errata_count_low),
+                    str(errata_count_bug),
+                    str(errata_count_enh),
+                    str(score),
+                    str(applicable_errata_count_sec),
+                    str(applicable_errata_count_cri),
+                    str(applicable_errata_count_imp),
+                    str(applicable_errata_count_mod),
+                    str(applicable_errata_count_low),
+                    str(applicable_errata_count_bug),
+                    str(applicable_errata_count_enh),
+                    str(applicable_score),
+                    str(content_view_name),
+                    str(cv_date),
+                    str(lifecycle_environment),
+                    str(subscription_os_release),
+                    str(os_release),
+                    str(arch),
+                    str(subscription_status),
+                    str(host["comment"]),
+                ]
+                         )
+            )
 
     available_file.closed
     applicable_file.closed
+
 
 if __name__ == "__main__":
 
